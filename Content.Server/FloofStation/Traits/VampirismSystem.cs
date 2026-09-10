8774c32277250cebe51752f0bd9258205244a522
Sydney Grayy <sydneygray64@protonmail.com>
2026-09-09 17:33:07 -0700

    Modified: VampirismSystem to work differently (INCOMPLETE)

--- Content.Server/FloofStation/Traits/VampirismSystem.cs
+++ Content.Server/FloofStation/Traits/VampirismSystem.cs
@@ -6,6 +6,7 @@ using Content.Shared.Body.Systems;
 using Content.Shared.Metabolism;
 
 
+
 namespace Content.Server.Floofstation.Traits;
 
 /// <summary>
@@ -28,36 +29,10 @@ public sealed class VampirismSystem : EntitySystem
         //Make sure the entity has the BloodSuckerComponent
         EnsureBloodSucker(ent);
 
-        /*
-        This method is only used because it's essentially a drop-in replacement for how the code originally worked.
-        There is very much a better way to do this and it should be done that way... eventually. When that is, I'm
-        unsure. Nubody is very unstable and an implementation of this system that adheres entirely to its principles
-        seems iffy to me. I think I'm stuck between a rock and a hard place here. It seems to be there can either be
-        an outdated implementation using bad practices by forcefully setting the fields of two components and using
-        obsolete methods, or be at the mercy of whatever changes come down to Nubody. The way I'm thinking, it will
-        need yet another rewrite. In the meantime, this horrible little implementation allows us to choose when that
-        will be done.
-        */
-        if (!TryComp<BodyComponent>(ent, out var body)
-		    || !_bodySystem.TryGetOrgansWithComponent<MetabolizerComponent>((ent, body), out var metabolizingOrgans))
-            return;
+        var remEv = new BodySystem.WantOrganRemovedEvent(ent, regularStomach);
+        var addEv = new BodySystem.WantOrganAddedEvent(ent, vampStomach);
 
-        foreach (var metabolizingOrgan in metabolizingOrgans)
-        {
-            //Try and get the StomachComponent
-            if (!TryComp<StomachComponent>(metabolizingOrgan, out var stomach))
-                continue;
-            //Try and get the MetabolizerComponent
-            if (!TryComp<MetabolizerComponent>(metabolizingOrgan, out var metabolizer))
-                continue;
-
-            //Set the MetabolizerComponent's MetabolizerTypes to VampireComponent's field instead
-            metabolizer.MetabolizerTypes = ent.Comp.MetabolizerPrototypes;
-
-            //Do much the same to the StomachComponent's SpecialDigestible field.
-            if (ent.Comp.SpecialDigestible is {} whitelist)
-                stomach.SpecialDigestible = whitelist;
-        }
+    }
     }
 
     private void EnsureBloodSucker(Entity<VampirismComponent> uid)
